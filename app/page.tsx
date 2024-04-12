81c9e22db81d873aa5b71a4d63c212e42d00c8aa
LeeTrent <lee.trent.1@gmail.com>
2024-04-11 21:53:24 -0500

    Got auth working

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 import { Button } from '@nextui-org/react';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
-    <Button>Click me!!!</Button>
+    <div>
+    <form action={actions.signIn}>
+      <Button type="submit">Sign In</Button>
+    </form>
+     <form action={actions.signOut}>
+      <Button type="submit">Sign Out</Button>
+    </form>
+    {
+      session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>
+    }
+    </div>
   );
 }
