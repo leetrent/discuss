51e7d46a95568dbf09d1b2b0eef348e8c1252a38
LeeTrent <lee.trent.1@gmail.com>
2024-04-14 20:53:46 -0500

    Completed page header rendering

--- components/header.tsx
+++ components/header.tsx
@@ -9,9 +9,25 @@ import {
     Avatar
 } from '@nextui-org/react';
 import { auth } from '@/auth';
+import React from 'react';
 
 export default async function Header() {
     const session = await auth();
+    let authContent: React.ReactNode;
+    if (session?.user) {
+        authContent = <Avatar src={session.user.image || ''} />;
+    } else {
+        authContent = (
+            <>
+                <NavbarItem>
+                    <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button type="submit" color="primary" variant="flat">Sign Up</Button>
+                </NavbarItem>
+            </>
+        );
+    }
     return (
         <Navbar className="shadow mb-6">
             <NavbarBrand>
@@ -20,13 +36,7 @@ export default async function Header() {
             <NavbarContent justify="center">
                 <NavbarItem><Input/></NavbarItem>
             </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    {
-                        session?.user ? <div>Signed In</div> : <div>Signed Out</div>
-                    }
-                </NavbarItem>
-            </NavbarContent>
+            <NavbarContent justify="end">{authContent}</NavbarContent>
         </Navbar>
     );
 }
