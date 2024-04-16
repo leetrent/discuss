a6f322ff479ab8f82a1b3257e2e3c8a034060321
LeeTrent <lee.trent.1@gmail.com>
2024-04-15 21:04:51 -0500

    Cleaned up home page

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,10 @@ import {
     NavbarItem,
     Input,
     Button,
-    Avatar
+    Avatar,
+    Popover,
+    PopoverTrigger,
+    PopoverContent
 } from '@nextui-org/react';
 import { auth } from '@/auth';
 import React from 'react';
@@ -16,7 +19,20 @@ export default async function Header() {
     const session = await auth();
     let authContent: React.ReactNode;
     if (session?.user) {
-        authContent = <Avatar src={session.user.image || ''} />;
+        authContent = (
+            <Popover placement="left">
+                <PopoverTrigger>
+                    <Avatar src={session.user.image || ''} />
+                </PopoverTrigger>
+                <PopoverContent>
+                    <div className="p-4">
+                        <form action={actions.signOut}>
+                            <Button type="submit">Sign Out</Button>
+                        </form>
+                    </div>
+                </PopoverContent>
+            </Popover>
+        );
     } else {
         authContent = (
             <>
