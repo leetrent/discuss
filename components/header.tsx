8b4ff2ee7f5fafc82398c58547dd50dcbcc87472
LeeTrent <lee.trent.1@gmail.com>
2024-04-15 20:40:14 -0500

    Enabled login buttons in page hearer

--- components/header.tsx
+++ components/header.tsx
@@ -10,6 +10,7 @@ import {
 } from '@nextui-org/react';
 import { auth } from '@/auth';
 import React from 'react';
+import * as actions from '@/actions';
 
 export default async function Header() {
     const session = await auth();
@@ -20,10 +21,14 @@ export default async function Header() {
         authContent = (
             <>
                 <NavbarItem>
-                    <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
+                    <form action={actions.signIn}>
+                        <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
+                    </form>
                 </NavbarItem>
                 <NavbarItem>
-                    <Button type="submit" color="primary" variant="flat">Sign Up</Button>
+                    <form action={actions.signIn}>
+                        <Button type="submit" color="primary" variant="flat">Sign Up</Button>
+                    </form>
                 </NavbarItem>
             </>
         );
