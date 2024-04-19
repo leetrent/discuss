71ff0810252382fa8c502d133e40cdee89b68b8f
LeeTrent <lee.trent.1@gmail.com>
2024-04-18 20:47:09 -0500

    Creating topic create form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import TopicCreateForm from '@/components/topics/topic-create-form';
+
 export default  function Home() {
   return (
-    <div>Home Page</div>
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top  Posts</h1>
+      </div>
+      <TopicCreateForm />
+    </div>
   );
 }
