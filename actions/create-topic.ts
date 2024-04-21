c39ba0c1d09f05d03ff84130e0cd81f370b5edbe
LeeTrent <lee.trent.1@gmail.com>
2024-04-20 20:49:22 -0500

    Partially completed create topic server action

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,5 +1,11 @@
 'use server';
 
-export async function createTopic() {}
+export async function createTopic(formData: FormData) {
+    const name = formData.get("name");
+    const description = formData.get("description");
+
+    console.log("createTopic() => (name):", name);
+    console.log("createTopic() => (description):", description);
+}
 
 // TODO: Revalidate the home page after creating a topic.
