41157e78eb5c1fbc437d8d2289d53a7cfcd8ad10
LeeTrent <lee.trent.1@gmail.com>
2024-04-21 20:43:49 -0500

    Installed zod

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,11 +1,21 @@
 'use server';
+import { z } from 'zod';
+
+const createTopicsSchema = z.object({
+    name: z.string().min(3).regex(/^[a-z-]+$/, {message: "Must be lowercase latters or dashes wihout spaces"}),
+    description: z.string().min(10)
+});
 
 export async function createTopic(formData: FormData) {
-    const name = formData.get("name");
-    const description = formData.get("description");
+    const result = createTopicsSchema.safeParse({
+        name: formData.get("name"),
+        description: formData.get("description")
 
-    console.log("createTopic() => (name):", name);
-    console.log("createTopic() => (description):", description);
+    });
+    if (!result.success) {
+        console.log(result.error.flatten().fieldErrors);
+    }
+
 }
 
 // TODO: Revalidate the home page after creating a topic.
